fab96b5e6deea48a3e1e0e19506326b138938d01
ssebadduka <seba.jamir@gmail.com>
2022-02-14 15:25:55 +0300

    edited static2

--- UK_Movies_Lib/settings.py
+++ UK_Movies_Lib/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'django_filters',
     'widget_tweaks',
+    'storages',
     
 ]
 
@@ -122,8 +123,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT='/static/'
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'film/static')]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
